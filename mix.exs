94f870e0b35bf37554a6c161435b5d2ba11cc64e
Shakarim Sapa <info@relitech.kz>
2021-10-07 19:20:14 +0600

    update hex package data

--- mix.exs
+++ mix.exs
@@ -48,7 +48,7 @@ defmodule Sphynx.MixProject do
   defp package() do
     [
       # These are the default files included in the package
-      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE* license* CHANGELOG* changelog* src),
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE* CHANGELOG*),
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/Shakarim/sphynx"}
     ]
